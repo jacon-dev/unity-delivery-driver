d67a62b5d5b0fe5939652feadbb459b61c9590e2
James Conlin <jacon.dev@outlook.com>
2023-08-27 13:10:05 +0100

    added variables for move speed and turn speed for the car

--- Delivery Driver/Assets/Driver.cs
+++ Delivery Driver/Assets/Driver.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    float turnSpeed = 1f;
+    float moveSpeed = 0.01f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0, turnSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
